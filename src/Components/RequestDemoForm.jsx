0b01af310a3a301bc644f309685da203589b6b68
Element27 <olasocials@gmail.com>
2025-09-22 12:57:07 +0100

    feat: add lead gen form modal

--- src/Components/RequestDemoForm.jsx
+++ src/Components/RequestDemoForm.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { IoCloseCircle } from 'react-icons/io5'
 
 export default function RequestDemoForm() {
 
@@ -21,3 +22,26 @@ export default function RequestDemoForm() {
     </div>
   )
 }
+
+export function LeadGenForm({ setIsOpen }) {
+  return (
+    <div className='fixed top-0 left-0 z-[1000] w-full h-screen bg-black/50 flex justify-center items-center'>
+      <div className='relative w-full h-[90%] mx-auto my-16 overflow-hidden font-bold text-center text-white bg-white md:w-2/3 lg:w-1/2'>
+
+        <IoCloseCircle onClick={() => setIsOpen(false)} className='absolute text-base text-red-500 cursor-pointer top-4 right-4' />
+
+        <div className="flex justify-center w-full h-full">
+          <iframe
+            title="Jotform Form"
+            src="https://form.jotform.com/Admintroohq/goGrubcoleads"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import LandingPage from "./LandingPage";
+import { LeadGenForm } from "@/Components/RequestDemoForm";
+import { useParams, useSearchParams } from "next/navigation";
 
 const Home = () => {
+
+  const params = useSearchParams();
+  const leads = params.get("leads");
+
+  const [showLeadGenModal, setShowLeadGenModal] = useState(leads);
+
+  console.log('showLeadGenModal', showLeadGenModal)
   return (
     <div className=" font-DMSans">
+      {showLeadGenModal && <LeadGenForm setIsOpen={setShowLeadGenModal} />}
       <LandingPage />
     </div>
   );
